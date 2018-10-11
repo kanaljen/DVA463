75a0c977c17c1a9401d288f605a168cc115a5615
PeterAndersson <peteranderssonkolback@gmail.com>
2018-10-11 14:12:16 +0200

    added t-test

--- INL2/INL2.2 - Data Analysis/R/analysis.R
+++ INL2/INL2.2 - Data Analysis/R/analysis.R
@@ -1,9 +1,17 @@
 analysis <- read.csv("integrated.csv",head=TRUE,sep=";")
 attach(analysis)
 
-wilcox.test(IDEA_time, IDEB_time)
+test_wilcox <- wilcox.test(IDEA_time, IDEB_time, paired = TRUE)
 
-#pdf("integrated.pdf",width=5,height=5)
+test_t_test <- t.test(IDEA_time, IDEB_time, paired = TRUE)
+
+d <- with(analysis, IDEA_time - IDEB_time)
+test_shapiro <- shapiro.test(d)
+
+pdf("integrated.pdf",width=5,height=5)
+
+boxplot(d,
+        las = 1,ylab="time (min)")
 
 # Boxplot for both IDEs
 boxplot(IDEA_time, IDEB_time,
@@ -33,4 +41,10 @@ plot(sort(IDEA_time),type="b",
 lines(sort(IDEB_time),col="red",type="b",pch=1)
 legend(1, 170, legend=c("IDE-A", "IDE-B"),
        col=c("green", "red"), lty=1, cex=0.8)
-#dev.off()
+
+# Histogram
+hist(d,xlab="time difference (min)",
+     ylab = "No subjects",
+     main="")
+
+dev.off()
